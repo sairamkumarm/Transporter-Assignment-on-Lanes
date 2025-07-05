73105de749e5daa3ec2b9c9355cab3b9fe2fedcb
sairamkumarm21 <sairamkumarm21@gmail.com>
2025-07-05 12:35:57 +0530

    init

---
+++ src/main/java/com/freightfox_sairamkumarm/transporter_assignment_on_lanes/TransporterAssignmentOnLanesApplication.java
@@ -0,0 +1,13 @@
+package com.freightfox_sairamkumarm.transporter_assignment_on_lanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporterAssignmentOnLanesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporterAssignmentOnLanesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freightfox_sairamkumarm/transporter_assignment_on_lanes/TransporterAssignmentOnLanesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freightfox_sairamkumarm.transporter_assignment_on_lanes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporterAssignmentOnLanesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
